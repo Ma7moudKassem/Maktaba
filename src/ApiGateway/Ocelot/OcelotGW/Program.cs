b0da351f11dba5365f24138594d97f9cd86eb05b
Mahmoud Kassem <modykassem123@gmail.com>
2023-06-02 17:21:21 +0300

    Update Program.cs

--- src/ApiGateway/Ocelot/OcelotGW/Program.cs
+++ src/ApiGateway/Ocelot/OcelotGW/Program.cs
@@ -1,14 +1,9 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json");
 builder.Services.AddOcelot(builder.Configuration);
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
